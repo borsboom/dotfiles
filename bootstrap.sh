1d9757cbe426b732b81393c97b8ff80208ee14d1
Emanuel Borsboom <manny@epiphyte.ca>
2014-08-15 06:54:17 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 if [[ -x $(which git) ]]; then
     if [[ -d $HOME/.eb-dotfiles ]]; then
-        (cd $HOME/.eb-dotfiles; git pull)
+        (cd $HOME/.eb-dotfiles; git pull -q)
     else
         git clone https://github.com/borsboom/dotfiles.git $HOME/.eb-dotfiles
     fi
@@ -8,4 +8,4 @@ else
     mkdir $HOME/.eb-dotfiles
     curl -sSL https://github.com/borsboom/dotfiles/tarball/master|tar xzf - --strip-components=1 -C $HOME/.eb-dotfiles
 fi
-exec /bin/bash -i
+exec /bin/bash -i --rcfile $HOME/.eb-dotfiles/bashrc

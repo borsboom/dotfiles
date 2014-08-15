3e03e739b03eaca3226afeabed5f3d6ceb7d379c
Emanuel Borsboom <manny@epiphyte.ca>
2014-08-15 06:30:48 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,7 @@
-echo hello!
+if [[ -x $(which git) ]]; then
+    git clone https://github.com/borsboom/dotfiles.git ~/.eb-dotfiles
+else
+    mkdir ~/.eb-dotfiles
+    curl -sSL https://github.com/borsboom/dotfiles/tarball/master|tar xzf - --strip-components=1 -C ~/.eb-dotfiles
+fi
 exec /bin/bash -i
